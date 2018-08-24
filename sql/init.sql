f3ee7a427ebe024cd27640e7c494a44d6ea2a1e7
Miles Steele <miles@milessteele.com>
2018-08-23 20:50:38 -0400

    sql

--- sql/init.sql
+++ sql/init.sql
@@ -37,15 +37,16 @@ create table if not exists categories (
   alive bool not null,
   name text not null,
   ctime timestamp not null default current_timestamp,
-  primary key (fid, id),
+  primary key (fid, id)
 );
 
 create table if not exists transactions (
   id char(32) primary key,
   fid char(32) not null references frames, -- transaction always has a frame, but may not have a category yet
-  category char(32) references categories,
+  category char(32),
   alive bool not null,
-  ctime timestamp not null default current_timestamp
+  ctime timestamp not null default current_timestamp,
+  foreign key (category, fid) references categories
 );
 
 commit;
