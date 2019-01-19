06e3258dd0c2b96643000612fa2246b31003aab1
Jess Kenney <jess@jesskenney.com>
2019-01-18 22:05:35 -0500

    fix login styles

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,46 +4,49 @@ import { BrowserRouter, Link, Route, RouteComponentProps, Switch } from "react-r
 import App from "./app";
 import NoRoute from "./noroute";
 
-const Home = () => <div className="home">
+const Home = () => <main className="home">
     <h1>Dino Budgeter</h1>
-    <main>
+    <div>
     <Link className="button" to="/login">Log In</Link>{" "}
     <Link className="button" to="/signup">Sign Up</Link>
-    </main>
-</div>;
+    </div>
+</main>;
 
 const msg = (text: string) => () => <div>{text}</div>;
 
-const Login = (props: RouteComponentProps<{}>) => <main className="login"><form action="/login" method="post">
+const Login = (props: RouteComponentProps<{}>) => <main className="login">
+    <h1>Dino Budgeter</h1>
+    <form action="/login" method="post">
     <input type="hidden" name="redirect" value={props.location.search.substring("?redirectTo=".length)} />
     <Route path="/login/error" render={msg("There was an error. Fix it!")} />
     <div>
-        <label>Email:</label>
+        <label>Email:{' '}</label>
         <input type="text" name="username"/>
     </div>
     <div>
-        <label>Password:</label>
+        <label>Password:{' '}</label>
         <input type="password" name="password"/>
     </div>
     <div>
-        <input type="submit" value="Log In"/>
+        <input type="submit" value="Log In" className="button" />
     </div>
 </form></main>;
 
 const Signup = () => <main className="signup"><form action="/signup" method="post">
+    <h1>Dino Budgeter</h1>
     <Route path="/signup/no-password" render={msg("You must enter a password.")} />
     <Route path="/signup/invalid-email" render={msg("You must enter a valid email.")} />
     <Route path="/signup/user-exists" render={msg("There is already an account with that email.")} />
     <div>
-        <label>Username:</label>
+        <label>Username:{' '}</label>
         <input type="text" name="username"/>
     </div>
     <div>
-        <label>Password:</label>
+        <label>Password:{' '}</label>
         <input type="password" name="password"/>
     </div>
     <div>
-        <input type="submit" value="Sign Up"/>
+        <input type="submit" value="Sign Up" className="button" />
     </div>
 </form></main>;
 
