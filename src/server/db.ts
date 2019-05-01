76fc7e9ff71ebf83d48bd27b04d879ffac4566a3
Jess Kenney <jess@jesskenney.com>
2019-04-30 21:18:54 -0400

    add db error handler; UNTESTED

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,6 +1,27 @@
 import { IMain } from "pg-promise";
 import pgPromise from "pg-promise";
 
-const pgp: IMain = pgPromise();
+const pgp: IMain = pgPromise({
+    error: (err: any, e: pgPromise.EventContext) => {
+        let msg = "";
+        if (typeof err === 'string') {
+            msg = err;
+        } else if (err instanceof Error) {
+            msg = err.message;
+        }
+
+        if (e.query) {
+            if (e.params) {
+                msg = "Exception in DB query: " + e.query + " with params: " + JSON.stringify(e.params)
+                    + "\n" + msg;
+            } else {
+                msg = "Exception in DB query: " + e.query + "\n" + msg;
+            }
+        }
+
+        console.error(msg);
+        //console.error(err);
+    },
+});
 
 export default pgp(`postgres://budgeter:${process.env.PGPASSWORD}@127.0.0.1/budgeter`);
